2b5f7122173ff5b070a37fad21d06b77a6a1f0d1
zainWebDeveloper86 <zain.webdev86@gmail.com>
2026-09-26 01:28:02 +0500

    Fix: Use transaction pooler for serverless

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,13 +9,17 @@ const globalForPrisma = globalThis as unknown as {
 // make adapter
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
+  // pgbouncer mode (for Supabase transaction pooler)
+  max: 1, // in Serverless 1 connection per function
+  idleTimeoutMillis: 0,
+  connectionTimeoutMillis: 10000,
 });
 
 // build connection with postgresql database
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, 
+    adapter,
     log:
       process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
   });
